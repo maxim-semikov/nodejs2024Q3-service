d84669604b98f008a5360e8410e30777daeef25e
Maxim Semikov <maxim.semikov87@gmail.com>
2024-11-09 19:57:52 +0500

    init: install dependencies and use env var

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 4000;
+
+  await app.listen(port);
 }
 bootstrap();
